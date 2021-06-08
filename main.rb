7ecbe7d3d265ec0405d88cb8d6234823f5f1065c
Murilo-Luciano <murilo.luciano123@gmail.com>
2021-06-08 09:03:32 -0400

    main.rb

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative 'brokerage_class'
+
+
+print "Enter Ticker: "
+ticker = gets()
+print "\nEnter Price: "
+price = gets()
+print "\nEnter Quantity: "
+quantity = gets()
+
+brk = Brokerage_note.new(ticker, price.to_f, quantity.to_i)
+print("Investment: ", brk.investment)
+print("\nBrokeage: ", brk.brokerage)
+print("\nEmoluments: ", brk.emoluments)
+print("\nValue: ", brk.value)
